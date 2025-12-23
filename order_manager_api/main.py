6cf2693988246f5211c3b599af2f9024d6dfa5aa
Carlos Calgaro <carloscalgarof@gmail.com>
2025-12-23 18:16:48 -0300

    feat: Adicionado path /api

--- order_manager_api/main.py
+++ order_manager_api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 from order_manager_api.api.main import api_router
 from order_manager_api.core.db import engine, init_db
 from sqlmodel import Session
@@ -9,5 +9,8 @@ with Session(engine) as session:
       init_db(session)
 
 app = FastAPI()
-      
-app.include_router(api_router)
+
+router = APIRouter(prefix="/api", tags=["api"])
+
+router.include_router(api_router)
+app.include_router(router)
